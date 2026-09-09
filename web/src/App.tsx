ce89e284b4f3cda70763f2ed160f0c2996e91bde
Arturo Barajas <basaar.24@gmail.com>
2026-09-09 07:27:38 -0600

    feat(parcial01) - add activity type

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
 
   useEffect(() => {
     fetch('https://localhost:5001/api/v1/events')
@@ -15,7 +15,7 @@ function App() {
     <div>
       <h3 style={{ color: 'red' }}>Events Hub</h3>
       <ul>
-        {activities.map((activity) => (
+        {activities.map((activity: Activity) => (
           <li key={activity.id}>{activity.title}</li>
         ))}
       </ul>
