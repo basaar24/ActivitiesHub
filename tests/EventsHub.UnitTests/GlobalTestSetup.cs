8241e2434336f7276cbd29fd1f78baa59f22aee5
Arturo Barajas <basaar.24@gmail.com>
2026-09-15 08:01:17 -0600

    feat(parcial02) - add unit tests

---
+++ tests/EventsHub.UnitTests/GlobalTestSetup.cs
@@ -0,0 +1,28 @@
+using EventsHub.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventsHub.UnitTests;
+
+[SetUpFixture]
+public class GlobalTestSetup
+{
+    public static AppDbContext AppDbContext { get; private set; }
+
+    [OneTimeSetUp]
+    public async Task Setup()
+    {
+        DbContextOptions<AppDbContext> options = 
+            new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlite("Data source=eventshub.db")
+                .Options;
+        AppDbContext = new AppDbContext(options);
+        await AppDbContext.Database.MigrateAsync();
+        await DbInitializer.SeedDataAsync(AppDbContext);
+    }
+
+    [OneTimeTearDown]
+    public async Task TearDown()
+    {
+        await AppDbContext.DisposeAsync();
+    }
+}
