543a070451f582824b175fc48b0c09ba80a779ad
Arturo Barajas <basaar.24@gmail.com>
2026-09-22 07:45:51 -0600

    feat(parcial02) - remove primaty constructor

--- src/EventsHub.Api/Controllers/EventsHubBaseController.cs
+++ src/EventsHub.Api/Controllers/EventsHubBaseController.cs
@@ -1,7 +1,15 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace EventsHub.Api.Controllers;
 
 [Route("api/v1/[controller]")]
 [ApiController]
-public class EventsHubBaseController : ControllerBase { }
+public class EventsHubBaseController : ControllerBase
+{
+    private IMediator? _mediator;
+
+    protected IMediator Mediator =>
+        _mediator ??= HttpContext.RequestServices.GetService<IMediator>()
+            ?? throw new InvalidOperationException("IMediator service in unavailabler");
+}
